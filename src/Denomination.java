457b64189715b4c4ac351275a9fd0981d8717e27
wadolp <williamisadolph@gmail.com>
2025-01-14 13:10:41 -0600

    Added files and started work on Purse

---
+++ src/Denomination.java
@@ -0,0 +1,2 @@
+public record Denomination(String name,double amt,String form, String img) {
+}
---
+++ src/Purse.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Purse {
+    //construction of Hash Map
+    Map<Denomination,Integer> map = new HashMap<Denomination,Integer>();
+
+    //Function to add currency to Purse
+    public void add(Denomination type, int num) {
+
+    }
+
+    //Function to remove currency and return amount in purse.
+    public double remove(Denomination type, int num) {
+        double final_total = 0;
+        final_total = this.getValue();
+        return final_total;
+    }
+
+    //Method to return amount in purse
+    public double getValue(){
+        double total = 0;
+        //Function to add up total amount
+        //***************************************************************************
+        return total;
+    }
+}
